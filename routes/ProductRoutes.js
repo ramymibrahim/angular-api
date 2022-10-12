0f0dccbc208c94b0452ebed0f1e2b1b1fd990772
Ramy Ibrahim <r.ibrahim@prosylab.com>
2022-10-12 16:27:27 +0200

    T

--- routes/ProductRoutes.js
+++ routes/ProductRoutes.js
@@ -14,10 +14,10 @@ const router = express.Router();
 
 router.get("/", getAllProducts);
 router.post("/", createProduct);
-router.get("/:id", getProductById);
-router.put("/:id", updateProduct);
-router.delete("/:id", deleteProduct);
 router.get("/getFeatured", getFeaturedProducts);
 router.get("/getRecent", getRecentProducts);
 router.get("/getByCategoryId/:id", getProductsByCategoryId);
+router.get("/:id", getProductById);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
 module.exports = router;
